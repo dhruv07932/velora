f946b905e89f072111709b094f4699ccd31c2aa1
Dhruv Sharma <dhruv07932@gmail.com>
2026-08-03 15:03:05 +0530

    Fix backend API routes

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -1,22 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://dhruv07932_db_user:dhruv1234@cluster0.92fvzza.mongodb.net/?appName=Cluster0")
-.then(() => {
+
+// Routes
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{
     console.log("MongoDB Connected");
 })
-.catch((err) => {
+.catch((err)=>{
     console.log(err);
 });
+
+
 
 app.get("/", (req,res)=>{
     res.send("Velora Backend Running");
 });
 
-app.listen(5000, ()=>{
+
+
+app.listen(5000,()=>{
     console.log("Server running on port 5000");
 });
